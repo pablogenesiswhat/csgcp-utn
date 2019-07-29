14b5e3152d6b48cdcb1a3514de4cbf8a3327a4f1
Pablo Rodriguez <pablo@genesiswhat.io>
2019-07-29 04:31:06 +0000

    Agregacion de funciones para almacenado de datos en firestore

--- appengine/app.js
+++ appengine/app.js
@@ -1,19 +1,47 @@
 const express = require("express");
 const cors = require("cors");
+//coneccion a una base de datos dentro de firestore
+const db = require("./lib/firestore");
 
 const app = express();
 
 app.use(cors({ origin: true }));
 
+// en esta ruta de firestore se iran todos los valores que agreguemos cpor get dentro de set
+const collection = db.collection("demoUtn").doc("mensajes");
+
 // rutas de app engine con express
-// index
+// Ruta inicial
 app.get("/", (req, res) => {
+  res.send("Ruta inicial");
+});
+
+// cargar datos por peticion get 
+app.get("/set", (req, res) => {
+  const value = req.query.valor || null;
+  collection.set({
+   valor: value
+  });
   res.status(200).send("ruta de inicio");
 });
 
 // /get
 app.get("/get", (req, res) => {
-  res.status(200).send("peticion get");
+  collection.get()
+    .then((doc) => {
+	if (doc.exists) {
+	  const result = JSON.stringify(doc.data());
+	  console.log(result);
+	  res.status(200).send("revisar en el log");
+	}
+	else 
+	  res.status(200).send("Sin datos");
+    })
+    .catch(err => {
+	console.log(err);
+	res.status(200).send(err);
+    });
+  res.status(200).send(db);
 });
 
 // levatar servicio en puerto 8080
