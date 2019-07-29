4ca0ae6410018092f3e1b992aed37a273ba25597
Pablo Rodriguez <pablo@genesiswhat.io>
2019-07-29 07:13:44 +0000

    Comment index.js => appengine add second function on firebase

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({ origin: true }));
+
+// ruta de la funcion http
+app.get("/inicio", (req, res) => {
+   res.status(200).send("Inicio de app");
+});
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// levantar servicio de rutas en cloud functions 
+exports.app = functions.https.onRequest(app);
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+
+//exports.helloWorld = functions.https.onRequest((req, res) => {
+//  res.send("Hello from Firebase!");
+//});
