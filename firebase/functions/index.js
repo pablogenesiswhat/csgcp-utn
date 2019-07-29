fd5253cd9f30b20867c341d05c03e44b0b0aa83e
Pablo Rodriguez <pablo@genesiswhat.io>
2019-07-29 05:36:53 +0000

    firebase init

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,15 +1,8 @@
-const functions = require("firebase-functions");
-const express = require("express");
-const cors = require("cors");
+const functions = require('firebase-functions');
 
-const app = express();
-
-app.use(cors({ origin: true }));
-
-// ruta de la funcion http
-app.get("/inicio", (req, res) => {
-  res.status(200).send("Inicio de app");
-});
-
-// levantar servicio de rutas en cloud functions 
-exports.app = functions.https.onRequest(app);
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
